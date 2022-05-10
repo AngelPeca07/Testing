7ce30dbfb58a3f4d027b37e7793dfb81c7ff6cc3
Angel Peña <jose.angpc@gmail.com>
2022-05-10 15:04:45 -0500

    Challenge 1 UI - Completed

--- cypress/integration/Testing/ui/mainPage.test.js
+++ cypress/integration/Testing/ui/mainPage.test.js
@@ -1,27 +1,51 @@
+describe('This will go to the amazon page and validate random items', () => {
+  beforeEach(() => {
+    cy.visit('www.amazon.com');
+  });
 
+  it('Should open the main page', () => {
+    cy.get('#nav-logo-sprites', { timeout: 2000 }).should('be.visible');
+    cy.get('span').should('have.class', 'icp-nav-flag-us');
+  });
 
-
-describe("This will go to the amazon page and validate random items", () => {
-    beforeEach(() => {
-        cy.visit('www.amazon.com');
+  it('Should look for products', () => {
+    var items = ['iphone X', 'iphone 8', 'iphone'];
+    var result = [];
+    var text = '';
+    cy.get('.hm-icon', { timeout: 2000 }).click();
+    cy.wait(2000);
+    items.forEach((element) => {
+      cy.get('input[type="text"]').type(element);
+      cy.get('#nav-search-submit-button').click();
+      cy.get('img[data-image-index="2"]', { multiple: true }).first().click();
+      cy.get('.a-offscreen').then(($aprice) => {
+        text = $aprice.eq(0).text();
+        result.push(parseFloat(text.substring(1)));
+        var sum = result.reduce((a, b) => a + b, 0);
+        cy.log(`[TOTAL:${sum}](http://example.com)`);
       });
-
-    it("Should open the main page", () => {
-        cy.get('#nav-logo-sprites', {timeout:2000}).should('be.visible');
-        cy.get('span').should('have.class','icp-nav-flag-us');
+      cy.get('#add-to-cart-button', { timeout: 2000 }).click();
     });
-
-    it("Should look for products", () => {
-        let items = ['iphone', 'iphone', 'iphone'];
-        cy.get('.hm-icon', {timeout:2000}).click();
-        cy.wait(2000);
-        items.forEach(element => {
-            cy.get('input[type="text"]').type(element);
-            cy.get('#nav-search-submit-button').click();
-            cy.get('img[data-image-index="2"]',{ multiple: true }).first().click();
-            cy.get('#add-to-cart-button',{timeout:2000}).click();
-        })
-
-        //cy.get('#sw-gtc > .a-button-inner > .a-button-text').click();
+    cy.get('#nav-cart-count').then(($num) => {
+      var value = $num.text();
+      var txt = parseInt(value);
+      expect(txt).to.equal(items.length);
     });
+    cy.get('#nav-cart-count').should('have.length.greaterThan', 0);
+    cy.get('#sc-buy-box-ptc-button').should(
+      'have.css',
+      'color',
+      'rgb(15, 17, 17)'
+    );
+  });
+
+  it('Should validate the color of the elements', () => {
+    cy.get('#nav-xshop').should('have.css', 'color', 'rgb(15, 17, 17)');
+    cy.get('#nav-tools').should('have.css', 'color', 'rgb(15, 17, 17)');
+    cy.get('#nav-search-submit-button').should(
+      'have.css',
+      'color',
+      'rgb(255, 255, 255)'
+    );
+  });
 });
